56c42c149813ad469efaa19039a8e5b81f40ae7f
TFGoscar <148482181+TFGoscar@users.noreply.github.com>
2025-01-17 10:41:43 +0100

    Add files via upload

---
+++ ou.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+echo "Organizative Units inside LDAP"
+echo ""
+# Función que recibe una lista con las unidades organizativas del directorio en formato dn
+function mostrarUnidades() {
+    for datos in $@; do
+        # Obtener el nombre de la unidad organizativa
+        nombreOU=$(echo $datos | sed 's/^dn: ou=//g' | cut -d"," -f1 | sed 's/ou=//g')
+        
+        echo "- $nombreOU"
+
+        # Crear patrón para buscar subunidades organizativas dentro de esta unidad
+        echo "^dn: ou=[[:alnum:]]+,$datos$" > patron
+        subunidades=$(slapcat | grep -Eif patron | sed 's/^dn: ou=//g' | cut -d"," -f1)
+
+        # Mostrar subunidades si existen
+        if [ "$subunidades" != "" ]; then
+
+            for sub in $subunidades; do
+                echo "$sub"
+            done
+        fi
+
+        echo ""
+    done
+
+    # Limpiar el archivo temporal de patrones
+    rm patron
+}
+
+# Obtener todas las unidades organizativas (OUs)
+unidades=$(slapcat | grep "^dn: ou=" | sed 's/^dn: //g')
+
+# Procesar las unidades organizativas
+mostrarUnidades $unidades
