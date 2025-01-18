5b554d49224b2e2836cb37504a0f9209ebd60a8c
TFGoscar <148482181+TFGoscar@users.noreply.github.com>
2025-01-18 20:33:13 +0100

    Update usr.sh

--- usr.sh
+++ usr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-groups=$(ldapsearch -xLLL -b "dc=midominio11,dc=local" objectClass=posixAccount | grep cn: | awk '{print $2}')
+users=$(ldapsearch -xLLL -b "dc=midominio11,dc=local" objectClass=posixAccount | grep cn: | awk '{print $2}')
 echo -e "[Name]                        [UID]"
 
-for i in $groups
+for i in $users
 do
     uid=$(ldapsearch -xLLL -b "dc=midominio11,dc=local" cn=$i uidNumber | grep uidNumber: | awk '{print $2}')
     # Usamos printf para formatear la salida
