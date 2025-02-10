200d7becf51a48ce85ce694a4e34a6ec78c46f0f
TFGoscar <148482181+TFGoscar@users.noreply.github.com>
2025-02-10 10:01:48 +0100

    Update grp.sh

--- bins/grp.sh
+++ bins/grp.sh
@@ -3,7 +3,7 @@ dc1=$(cat /etc/lsdap/data.conf | grep "fqdn" | awk -F '=' '{print $2}' | awk -F 
 dc2=$(cat /etc/lsdap/data.conf | grep "fqdn" | awk -F '=' '{print $2}' | awk -F '.' '{print $3}')
 
 groups=$(ldapsearch -xLLL -b "dc=$dc1,dc=$dc2" objectClass=posixGroup | grep cn: | awk '{print $2}')
-echo -e "[Name]                        [GID]"
+echo "[Name]                        [GID]"
 
 for i in $groups
 do
